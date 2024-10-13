8605c53336279801d2ae87ffa462cbc0abbe81ff
SubVlad <57858104+SubVlad@users.noreply.github.com>
2024-10-14 03:26:31 +0500

    lesson3

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+const schemeSvg = document.querySelector(".scheme_svg");
+const totalPriceTag = document.querySelector(".price_total");
+let cost = 800;
+let totalPrice = 0;
+schemeSvg.addEventListener("click", (event) => {
+	if(!event.target.classList.contains("booked")){
+		event.target.classList.toggle("active");
+		let totalSeats = schemeSvg.querySelectorAll(".active").length;
+		totalPrice = totalSeats * cost;
+		totalPriceTag.textContent = totalPrice;
+	}
+});
